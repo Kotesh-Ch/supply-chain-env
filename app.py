fabf5f7dd5e95ddb17f93d54d1e5ccceb31eb82a
Kotesh Ch <koteshch121@gmail.com>
2026-04-08 21:47:46 +0530

    Fix HF UI

--- app.py
+++ app.py
@@ -3,4 +3,7 @@ import gradio as gr
 def demo(x):
     return "Output: " + x
 
-gr.Interface(fn=demo, inputs="text", outputs="text").launch()
+iface = gr.Interface(fn=demo, inputs="text", outputs="text")
+
+if __name__ == "__main__":
+    iface.launch(server_name="0.0.0.0", server_port=7860)
